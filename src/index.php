9d8a1660e5c158fcaf75ce3a9fb964781f88aa8c
Dimitris Nikolaidis <dnikolaidis2@isc.tuc.gr>
2019-10-24 22:23:04 +0300

    Added two error reporting methods
    Non intrusive pre-placed alert
    Modal popup

--- src/index.php
+++ src/index.php
@@ -24,6 +24,7 @@
 
   </head>
   <body>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 
   <?php
     $login_active = true;
@@ -39,7 +40,7 @@
     {
       if (empty($_POST["username"])) 
       {
-        echo '<p>field username is required</p>';
+        display_error("Field username is required");
       } 
       else 
       {
@@ -48,26 +49,62 @@
 
       if (empty($_POST["password"])) 
       {
-        echo '<p>field passowrd is required</p>';
+        display_error("Field passowrd is required");
       }
       else 
       {
         $password = test_input($_POST["password"]);
       }
-
+      login_error("#login_alert", "test testing test");
       $pdo = new PDO('mysql:host=' . $_ENV["MYSQL_HOST"] . ';dbname='. $_ENV["MYSQL_DATABASE"] , $_ENV["MYSQL_USER"], $_ENV["MYSQL_PASSWORD"]);
       
     }
     else
     {
-      echo '<p>unsuported requst method</p>';
+      display_error("unsuported requst method.");
     }
 
-    function test_input($data) {
+    function login_error($id, $error_message)
+    {
+      echo '
+        <script type="text/javascript">
+          $(window).on("load",function(){
+            $("' . $id . '").text("' . $error_message . '");
+            $("' . $id . '").removeClass("invisible")
+          });
+        </script>
+      ';
+    }
+
+    function test_input($data) 
+    {
       $data = trim($data);
       $data = stripslashes($data);
       $data = htmlspecialchars($data);
       return $data;
+    }
+
+    function display_error($error_message) 
+    {
+      echo '
+        <div class="modal" tabindex="-1" role="dialog" id="error_modal">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title">Error occured</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <p>' . $error_message . '</p>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <script type="text/javascript">$(window).on("load",function(){$("#error_modal").modal("show");});</script>
+      ';
     }
   ?>
 
@@ -95,7 +132,7 @@
                 <label for="password" id="text-main">Password</label>
                 <input type="password" class="form-control" id="password" placeholder="Password" name="password" required>
               </div>
-              <div class="alert alert-danger invisible" role="alert">
+              <div class="alert alert-danger invisible" role="alert" id="login_alert">
               </div>
               <button type="submit" class="btn btn-primary btn-block">Log in</button>
             </form>
@@ -122,7 +159,7 @@
                 <label for="password" id="text-main">Password</label>
                 <input type="password" class="form-control" id="password" placeholder="Password" name="password" required>
               </div>
-              <div class="alert alert-danger invisible" role="alert">
+              <div class="alert alert-danger invisible" role="alert" id="singup_alert">
               </div>
               <button type="submit" class="btn btn-primary btn-block">Sing Up</button>
             </form>
@@ -133,7 +170,7 @@
     </div>
   </div>
 
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </body>

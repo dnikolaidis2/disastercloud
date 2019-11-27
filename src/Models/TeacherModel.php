a9dafc0375c97ad0fd934c98e4887df4ed778aeb
Dimitris Nikolaidis <dnikolaidis2@isc.tuc.gr>
2019-11-27 11:12:14 +0200

    Reformat code

--- src/Models/TeacherModel.php
+++ src/Models/TeacherModel.php
@@ -3,45 +3,45 @@ include 'Utils/Random.php';
 
 class Teacher
 {
-	public $id;
-	public $name;
-	public $surname;
-	public $username;
-	public $password;
-	public $email;
+    public $id;
+    public $name;
+    public $surname;
+    public $username;
+    public $password;
+    public $email;
 
-	public function __construct($id, $name, $surname, $username, $password, $email)
-	{
-		$this->id = $id;
-		$this->name = $name;
-		$this->surname = $surname;
-		$this->username = $username;
-		$this->password = $password;
-		$this->email = $email;
-	}
+    public function __construct($id, $name, $surname, $username, $password, $email)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->surname = $surname;
+        $this->username = $username;
+        $this->password = $password;
+        $this->email = $email;
+    }
 
-	public function __toString()
-	{
-		return "Teacher\n
+    public function __toString()
+    {
+        return "Teacher\n
 				ID: {$this->id}\n
 				NAME: {$this->name}\n
 				SURNAME: {$this->surname}\n
 				USERNAME: {$this->username}\n
 				PASSWORD: {$this->password}\n
 				EMAIL: {$this->email}\n";
-	}
+    }
 
-	public static function loadFromQuery($row)
-	{
-		$ret = new self($row["ID"],
-			$row["NAME"],
-			$row["SURNAME"],
-			$row["USERNAME"],
-			$row["PASSWORD"],
-			$row["EMAIL"]
-		);
-		return $ret;
-	}
+    public static function loadFromQuery($row)
+    {
+        $ret = new self($row["ID"],
+            $row["NAME"],
+            $row["SURNAME"],
+            $row["USERNAME"],
+            $row["PASSWORD"],
+            $row["EMAIL"]
+        );
+        return $ret;
+    }
 }
 
 class TeacherModel
@@ -55,43 +55,35 @@ class TeacherModel
 
     public function getTeacherByUsername($username)
     {
-    	$stmt = $this->db->prepare('SELECT * FROM Teachers WHERE USERNAME = :username LIMIT 1');
-    	if (!$stmt->bindParam(':username', $username, PDO::PARAM_STR))
-    	{
-    		return null;
-    	}
-
-        if (!$stmt->execute())
-        {
-        	return null;
+        $stmt = $this->db->prepare('SELECT * FROM Teachers WHERE USERNAME = :username LIMIT 1');
+        if (!$stmt->bindParam(':username', $username, PDO::PARAM_STR)) {
+            return null;
         }
-        else
-        {
-        	$result = $stmt->fetch(PDO::FETCH_ASSOC);
-        	if ($result !== false) {
-        		return Teacher::loadFromQuery($result);
-        	}	
+
+        if (!$stmt->execute()) {
+            return null;
+        } else {
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+            if ($result !== false) {
+                return Teacher::loadFromQuery($result);
+            }
         }
     }
 
     public function getTeacherByEmail($email)
     {
-    	$stmt = $this->db->prepare('SELECT * FROM Teachers WHERE EMAIL = :email LIMIT 1');
-    	if (!$stmt->bindParam(':email', $email, PDO::PARAM_STR))
-    	{
-    		return null;
-    	}
-
-        if (!$stmt->execute())
-        {
-        	return null;
+        $stmt = $this->db->prepare('SELECT * FROM Teachers WHERE EMAIL = :email LIMIT 1');
+        if (!$stmt->bindParam(':email', $email, PDO::PARAM_STR)) {
+            return null;
         }
-        else
-        {
-        	$result = $stmt->fetch(PDO::FETCH_ASSOC);
-        	if ($result !== false) {
-        		return Teacher::loadFromQuery($result);
-        	}	
+
+        if (!$stmt->execute()) {
+            return null;
+        } else {
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+            if ($result !== false) {
+                return Teacher::loadFromQuery($result);
+            }
         }
     }
 
@@ -104,8 +96,7 @@ class TeacherModel
         $stmt->bindParam(':username', $username, PDO::PARAM_STR);
         $stmt->bindParam(':password', password_hash($password, PASSWORD_BCRYPT), PDO::PARAM_STR);
         $stmt->bindParam(':email', $email, PDO::PARAM_STR);
-        if (!$stmt->execute())
-        {
+        if (!$stmt->execute()) {
             return false;
         }
 

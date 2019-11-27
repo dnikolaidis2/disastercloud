f801082b19bd61ce88ad03a1d5b581ba436f3409
Dimitris Nikolaidis <dnikolaidis2@isc.tuc.gr>
2019-11-27 11:17:57 +0200

    Updated Teacher.php
    -Added retrieval of students from db
    -Added rendering template with students

--- src/Teacher.php
+++ src/Teacher.php
@@ -1,16 +1,38 @@
 <?php
-	require 'vendor/autoload.php';
-	include 'Utils/Session.php';
-	include 'Utils/TemplateManager.php';
+require 'vendor/autoload.php';
+include 'Utils/Session.php';
+include 'Utils/TemplateManager.php';
+include 'Models/StudentModel.php';
 
-	$session = Session::getInstance();
+$session = Session::getInstance();
 
-	if (!($session->logedin && isset($session->username))) {
-	  header("Location: index.php");
-	}
+if (!($session->logedin && isset($session->username))) {
+    header("Location: index.php");
+}
 
-	$templates = new League\Plates\Engine('Templates');
-	$templateManager = TemplateManager::getInstance($templates);
+$templates = new League\Plates\Engine('Templates');
+$templateManager = TemplateManager::getInstance($templates);
 
-	$templateManager->renderTeacher();
+$pdo = null;
+try {
+    $pdo = new PDO('mysql:host=' . $_ENV["MYSQL_HOST"] . ';dbname=' . $_ENV["MYSQL_DATABASE"], $_ENV["MYSQL_USER"], $_ENV["MYSQL_PASSWORD"], array(
+        PDO::ATTR_PERSISTENT => true
+    ));
+} catch (PDOException $e) {
+//    TODO: Error
+}
+
+$studentModel = new StudentModel($pdo);
+$students = $studentModel->getAllStudents();
+if ($students === null){
+//    TODO: Error
+}
+
+$student_array = [];
+foreach ($students as $student) {
+    array_push($student_array, $student->pack());
+}
+
+$templateManager->renderTeacher($student_array);
+
 ?>

f3206a820b0f6a9f6e3882fbaff1248e041b8cd6
Dimitris Nikolaidis <dnikolaidis2@isc.tuc.gr>
2019-11-27 11:19:35 +0200

    Added EditStudent.php
    -Get method works
    -Get method gets student by id and shows edit template

--- src/EditStudent.php
+++ src/EditStudent.php
@@ -1,1 +1,52 @@
 <?php
+require 'vendor/autoload.php';
+include 'Utils/Session.php';
+include 'Utils/TemplateManager.php';
+include 'Models/StudentModel.php';
+include 'Utils/Sanitizer.php';
+
+$session = Session::getInstance();
+
+if (!($session->logedin && isset($session->username))) {
+    header("Location: index.php");
+}
+
+$templates = new League\Plates\Engine('Templates');
+$templateManager = TemplateManager::getInstance($templates);
+
+if($_SERVER['REQUEST_METHOD'] === 'GET') {
+    if (empty($_GET['id'])) {
+        //    TODO: Error
+        exit(1);
+    }
+    else
+    {
+        $id = test_input($_GET['id']);
+        if (strlen($id) != 10) {
+//            TODO: Error
+        }
+    }
+
+    $pdo = null;
+    try {
+        $pdo = new PDO('mysql:host=' . $_ENV["MYSQL_HOST"] . ';dbname=' . $_ENV["MYSQL_DATABASE"], $_ENV["MYSQL_USER"], $_ENV["MYSQL_PASSWORD"], array(
+            PDO::ATTR_PERSISTENT => true
+        ));
+    } catch (PDOException $e) {
+        //    TODO: Error
+    }
+
+    $studentModel = new StudentModel($pdo);
+    $student = $studentModel->getStudentById($id);
+
+    $templateManager->renderEditStudent($student->pack());
+
+}
+elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+}
+else{
+//    TODO: Error
+}
+
+?>
